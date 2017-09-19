7a0605cca37781a9d5959d00839ecda9172193e2
sahinbosnic <sahin.bosnic@hotmail.com>
2017-09-19 13:49:59 +0200

    Sökfunktionen skickar nu in queryn till HomeControllern

---
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.Web/Models/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WU16.BolindersBilAB.Web.Models
+{
+    public class HomeViewModel
+    {
+        [Required]
+        [DisplayName("SearchQuery")]
+        [DataType(DataType.Text)]
+        public string SearchQuery { get; set; }
+    }
+}
