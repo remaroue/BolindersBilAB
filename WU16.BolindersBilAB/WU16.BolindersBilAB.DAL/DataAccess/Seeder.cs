bb1c3f1bea70001bce4d53e6f399ade013d81147
Eric Eriksson <erceriksson@gmail.com>
2017-09-21 13:37:14 +0200

    Seeder class

---
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.DAL/DataAccess/Seeder.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+
+namespace WU16.BolindersBilAB.DAL.DataAccess
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedAttribute : Attribute
+    {
+        public SeederDataType DataType { get; private set; }
+
+        public SeedAttribute(SeederDataType dataType)
+        {
+            DataType = dataType;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedFromArrayAttribute : Attribute
+    {
+        public object[] Values { get; private set; }
+
+        public SeedFromArrayAttribute(object[] values)
+        {
+            Values = values;
+        }
+    }
+
+    public enum CharacterDescription
+    {
+        ZeroToNine,
+        AToZ
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedStringFromEnumArrayAttribute : Attribute
+    {
+        public CharacterDescription[] Description { get; private set; }
+
+        public SeedStringFromEnumArrayAttribute(CharacterDescription[] description)
+        {
+            Description = description;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedIgnoreAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedFixedValueAttribute : Attribute
+    {
+        public object Value { get; private set; }
+
+        public SeedFixedValueAttribute(object value)
+        {
+            Value = value;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedNumericValueAttribute : Attribute
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public SeedNumericValueAttribute(int min, int max)
+        {
+            IntMinMax = (min, max);
+        }
+    }
+
+    public enum SeederDataType
+    {
+        Name,
+        LoremIpsum,
+        PhoneNumber,
+        Year,
+        Now
+    }
+
+    public static class Seeder 
+    {
+        public static void Seed<T>(DbContext dbContext, int numberOfRows) where T : class, new()
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var rows = new T[numberOfRows];
+
+            for (int i = 0; i < numberOfRows; i++)
+                rows[i] = new T();
+
+            foreach (var property in properties)
+            {
+                var attributes = property.GetCustomAttributes(false);
+
+                if (attributes.Any(x => x is SeedIgnoreAttribute)) continue;
+
+                if (attributes.FirstOrDefault(x => x is SeedAttribute) is SeedAttribute sAttr)
+                {
+                    switch (sAttr.DataType)
+                    {
+                        case SeederDataType.LoremIpsum:
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis velit dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor diam et elit varius blandit. Curabitur convallis justo eu dui semper, sed aliquet elit sodales. Maecenas sapien justo, rhoncus lacinia vestibulum eget, sollicitudin non odio. Aliquam finibus augue in massa sollicitudin, ut lacinia tortor pulvinar. Vivamus sed auctor velit. Quisque sagittis elit at tellus suscipit, nec ultricies ligula mollis. Maecenas congue lacus quis metus efficitur gravida. Maecenas facilisis, est a fringilla consequat, nulla ante pharetra erat, quis venenatis tortor libero eu mi.");
+                            break;
+                        case SeederDataType.Name:
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], "Eric Eriksson");
+                            break;
+                        case SeederDataType.Now:
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], DateTime.Now);
+                            break;
+                        case SeederDataType.Year:
+                            var rand = new Random();
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], rand.Next(1960, 2021));
+                            break;
+                    }
+
+                    continue;
+                }
+                else if (attributes.FirstOrDefault(x => x is SeedNumericValueAttribute) is SeedNumericValueAttribute snAttr)
+                {
+                    if (property.PropertyType.IsPrimitive)
+                    {
+                        if (Convert.GetTypeCode(property.PropertyType) != TypeCode.Int32)
+                        {
+                            var rand = new Random();
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], rand.Next(snAttr.Min, snAttr.Max));
+                        }
+                        else if (Convert.GetTypeCode(property.PropertyType) != TypeCode.Double)
+                        {
+                            
+                        }
+                    }
+                    else if (property.PropertyType.Equals(typeof(decimal)))
+                    {
+                        
+                    }
+                }
+                else if (attributes.FirstOrDefault(x => x is SeedFixedValueAttribute) is SeedFixedValueAttribute sfAttr)
+                {
+                    for (int i = 0; i < numberOfRows; i++)
+                        property.SetValue(rows[i], sfAttr.Value);
+                }
+                else if (attributes.FirstOrDefault(x => x is SeedFromArrayAttribute) is SeedFromArrayAttribute sfaAttr)
+                {
+                    var rand = new Random();
+                    for (int i = 0; i < numberOfRows; i++)
+                        property.SetValue(rows[i], sfaAttr.Values[rand.Next(0, sfaAttr.Values.Length)]);
+                }
+                else if (attributes.FirstOrDefault(x => x is SeedStringFromEnumArrayAttribute) is SeedStringFromEnumArrayAttribute sfeAttr)
+                {
+                    var aToZ = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö" };
+                    var zeroToNine = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+                    var generated = new string[numberOfRows];
+                    for (int i = 0; i < numberOfRows; i++)
+                    {
+                        generated[i] = "";
+
+                        for (int x = 0; x < sfeAttr.Description.Length; x++)
+                        {
+                            var rand = new Random();
+                            switch(sfeAttr.Description[x])
+                            {
+                                case CharacterDescription.AToZ:
+                                    generated[i] += aToZ[rand.Next(0, aToZ.Length)];
+                                    break;
+                                case CharacterDescription.ZeroToNine:
+                                    generated[i] += zeroToNine[rand.Next(0, zeroToNine.Length)];
+                                    break;
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < numberOfRows; i++)
+                        property.SetValue(rows[i], generated[i]);
+                }
+                else
+                {
+                    if (property.PropertyType.IsPrimitive)
+                    {
+                        if(Convert.GetTypeCode(property.PropertyType) != TypeCode.Boolean)
+                        {
+                            var rand = new Random();
+                            for (int i = 0; i < numberOfRows; i++)
+                                property.SetValue(rows[i], Convert.ToBoolean(rand.Next(2)));
+                        }
+                    }
+                    else if (property.PropertyType.IsEnum)
+                    {
+                        var rand = new Random();
+                        var enums = Enum.GetNames(property.PropertyType).Select(x => Enum.Parse(property.PropertyType, x)).ToArray();
+
+                        for (int i = 0; i < numberOfRows; i++)
+                            property.SetValue(rows[i], enums[rand.Next(0, enums.Length)]);
+                    }
+                }
+                continue;
+            }
+
+            var set = dbContext.Set<T>();
+            set.AddRange(rows);
+            dbContext.SaveChanges();
+        }
+    }
+}
+
--- WU16.BolindersBilAB/WU16.BolindersBilAB.DAL/Models/Car.cs
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.DAL/Models/Car.cs
@@ -3,34 +3,66 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using WU16.BolindersBilAB.DAL.DataAccess;
 
 namespace WU16.BolindersBilAB.DAL.Models
 {
     public class Car
     {
         [Key]
+        [SeedStringFromEnumArray(new CharacterDescription[] { CharacterDescription.AToZ, CharacterDescription.AToZ, CharacterDescription.AToZ, CharacterDescription.ZeroToNine, CharacterDescription.ZeroToNine, CharacterDescription.ZeroToNine, CharacterDescription.ZeroToNine })]
         public string LicenseNumber { get; set; }
+
+        [SeedFromArray(new object[] { "Volvo", "BMW", "Audi", "Ford", "Mercedes-benz", "Volkswagen" })]
         [ForeignKey("CarBrand")]
         public string CarBrandId { get; set; }
+
+        [SeedIgnore]
         public virtual CarBrand CarBrand { get; set; }
+
+        [SeedFromArray(new object[] { "BB1", "BB2", "BB3" })]
         [ForeignKey("Location")]
         public string LocationId { get; set; }
+        [SeedIgnore]
         public virtual Location Location { get; set; }
+
+        [SeedFixedValue("test model")]
         public string Model { get; set; }
+
+        [Seed(SeederDataType.LoremIpsum)]
         public string Description { get; set; }
+
+        [Seed(SeederDataType.Year)]
         public int ModelYear { get; set; }
+
         public bool IsLeaseable { get; set; }
+
+        [SeedNumericValue(100, 20000)]
         public int Milage { get; set; }
+
+        [SeedNumericValue(30000, 150000)]
         public decimal Price { get; set; }
+
+        [SeedFromArray(new object[] {"grå", "grön", "mörkblå", "rengbåge"})]
         public string Color { get; set; }
+
+        [SeedNumericValue(80, 200)]
         public int HorsePower { get; set; }
+
         public bool Used { get; set; }
+
+        [Seed(SeederDataType.Now)]
         public DateTime CreationDate { get; set; }
-        public DateTime LastUpdated { get; set; }
+        [SeedIgnore]
+        public DateTime? LastUpdated { get; set; }
+        [SeedIgnore]
         public virtual ICollection<CarImage>  CarImages { get; set; }
+
         public CarType CarType { get; set; }
         public FuelType FuelType { get; set; }
         public Gearbox Gearbox { get; set; }
+
+        [SeedFixedValue("test|elsäten|laxskinsäten")]
         public string Equipment { get; set; }
     }
 }
