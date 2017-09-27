172b128e6f05b575cbb1b64ec400ed49e3909bb6
Eric Eriksson <erceriksson@gmail.com>
2017-09-27 13:22:32 +0200

    Fixed seeding deletion

---
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.DAL/Seeding/Attributes/SeedFixedValueAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WU16.BolindersBilAB.DAL.Seeding.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class SeedFixedValueAttribute : Attribute
+    {
+        public object Value { get; private set; }
+
+        public SeedFixedValueAttribute(object value)
+        {
+            Value = value;
+        }
+    }
+}
+
