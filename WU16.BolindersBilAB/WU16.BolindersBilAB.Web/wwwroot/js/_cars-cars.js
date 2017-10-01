dbfc8a8a7ee088019eec428a7f2b9bc608eb6667
remaroue <jacob.nodbrant@gmail.com>
2017-10-01 21:18:45 +0200

    Klickevent på hela panelen + scrollposition sparas vid omladdning.

--- WU16.BolindersBilAB/WU16.BolindersBilAB.Web/wwwroot/js/_cars-cars.js
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.Web/wwwroot/js/_cars-cars.js
@@ -3,5 +3,24 @@
     $('#carType').select2();
     $('#gearbox').select2();
 
-    //$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+    $('.car-list-item').on('click', function () {
+        var licenseNumber = $(this).data('id');
+        location.href = '/bil/' + licenseNumber
+    });
+    var timer;
+    $(window).on('scroll', function () {
+        if (timer)
+        {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function () {
+            sessionStorage.scrollTop = $(this).scrollTop();
+        }, 100);
+    });
+
+    if (sessionStorage.scrollTop !== "undefined")
+    {
+        $(window).scrollTop(sessionStorage.scrollTop);
+    }
+
 });
