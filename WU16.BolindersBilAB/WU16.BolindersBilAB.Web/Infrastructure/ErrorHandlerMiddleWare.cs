90495379442d3104b2ff788d0352d1317e7615bf
JNodbrant <jacob.nodbrant@gmail.com>
2017-10-06 18:26:49 +0200

    fix

--- WU16.BolindersBilAB/WU16.BolindersBilAB.Web/Infrastructure/ErrorHandlerMiddleWare.cs
+++ WU16.BolindersBilAB/WU16.BolindersBilAB.Web/Infrastructure/ErrorHandlerMiddleWare.cs
@@ -17,6 +17,7 @@ namespace WU16.BolindersBilAB.Web.Infrastructure
 
         public async Task Invoke(HttpContext context)
         {
+            await next.Invoke(context);
            if (context.Response.StatusCode == 404)
             {
                 context.Response.Redirect("/404");
